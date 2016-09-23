c814050a7f841c46b68a91f93f11b353d700b251
Alexander J. Hoffmann <alex@castalia.io>
2016-09-23 00:39:20 +0000

    First commit

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 var ViewModel = function() {
     this.name = ko.observable('Denver Concerts Tonight');
     this.imgSrc = ko.observable('http://svcdn.simpleviewinc.com/v3/cache/www_denver_org/0F2C4CC295D7D062AF82C1078420EB26.jpg');
-
     
     
 }
