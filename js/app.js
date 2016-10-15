fca9e8f4e96d9d2f5eb154eacaaa9aee9314c4fc
Alex Hoffmann <alex@castalia.io>
2016-10-15 11:45:25 -0600

    Info windows clean up

--- js/app.js
+++ js/app.js
@@ -6,10 +6,10 @@ venues = [];
 
 
 var ViewModel = function() {
-    
-    
+
+
     self.venues = ko.observableArray([
-      
+
         {name: 'Bluebird Theater', lat: 39.7403, lng: -104.9484, id: 10459},
         {name: 'Ogden Theater', lat: 39.7403, lng: -104.9753, id: 537},
         {name: 'Hi Dive', lat: 39.7163, lng: -104.9879, id: 2951},
@@ -20,9 +20,9 @@ var ViewModel = function() {
         {name: '3 Kings', lat: 39.7154, lng: -104.9873, id: 171428},
         {name: 'Cervantes Ballroom', lat: 39.7545, lng: -104.9787, id: 5668},
         {name: 'Gothic Theatre', lat: 39.6577, lng: -104.9878, id: 5229}
-       
-        
-        ]);
+
+
+    ]);
 
     // Selected venues filter
     selectedVenues = ko.observableArray([]);
@@ -34,62 +34,58 @@ var ViewModel = function() {
 
     // Songkick API
 
-    /*
-    $.getJSON("https://api.songkick.com/api/3.0/venues/10459/calendar.json?apikey=a3sNs8vQ4zpgjhCU", function(data)
-    {
-
-        concert = data.resultsPage.results.event[0].displayName
-        //console.log(data.resultsPage.results.event[1].displayName)
-        //console.log(data.resultsPage.results.event[2].displayName)
-    });
-
-    */
 
 
     // Loop through array to drop marker on each venue
-        self.venues().forEach(function (venue) {
+    self.venues().forEach(function (venue) {
 
-            marker = new google.maps.Marker({
+        marker = new google.maps.Marker({
             map: denverMap,
             position: new google.maps.LatLng(venue.lat, venue.lng),
             title: venue.name,
             animation: google.maps.Animation.DROP
-          });
+        });
 
 
-            // SONGKICK API
-            songkick = venue.id
-            var concert, concert1, concert2;
+        // SONGKICK API
+        songkick_id = venue.id
+        var concert, concert1, concert2;
 
-            $.getJSON("https://api.songkick.com/api/3.0/venues/" + songkick + "/calendar.json?apikey=a3sNs8vQ4zpgjhCU", function(data)
-            {
+        $.getJSON("https://api.songkick.com/api/3.0/venues/" + songkick_id + "/calendar.json?apikey=a3sNs8vQ4zpgjhCU", function (data) {
 
-                concert = data.resultsPage.results.event[0].displayName;
-                concert1 = data.resultsPage.results.event[1].displayName;
-                concert2 = data.resultsPage.results.event[2].displayName;
+            console.log("success")
 
-            });
+            concert = data.resultsPage.results.event[0].performance[0].artist.displayName;
+            concert1 = data.resultsPage.results.event[1].performance[0].artist.displayName;
+            concert2 = data.resultsPage.results.event[2].performance[0].artist.displayName;
 
-            venue.marker = marker
-          // Add listener for info windows on each map marker
-          marker.addListener('click', function () {
-              venue.marker.setAnimation(google.maps.Animation.BOUNCE);
-              setTimeout(function(){ venue.marker.setAnimation(null); }, 1450);
-              contentString =
-                  '<h1><a href=http://www.songkick.com/venues/' + venue.id + ' target="_blank">' + venue.name + '</a></h1>' +
-                  '<p>' + concert + '</p>' +
-                  '<p>' + concert1 + '</p>' +
-                  '<p>' + concert2 + '</p>'
+            date = data.resultsPage.results.event[0].start.date;
+            date1 = data.resultsPage.results.event[1].start.date;
+            date2 = data.resultsPage.results.event[2].start.date;
 
-              infowindow.setContent(contentString);
-              infowindow.open(denverMap, this);
 
-          });
+        });
+
+        venue.marker = marker
+        // Add listener for info windows on each map marker
+        marker.addListener('click', function () {
+            venue.marker.setAnimation(google.maps.Animation.BOUNCE);
+            setTimeout(function(){ venue.marker.setAnimation(null); }, 1450);
+            contentString =
+                '<h1><a href=http://www.songkick.com/venues/' + venue.id + ' target="_blank">' + venue.name + '</a></h1>' +
+                '<p><strong>' + date + '</strong>' + ' ' + concert + '</p>' +
+                '<p><strong>' + date1 + '</strong>' + ' ' + concert1 + '</p>' +
+                '<p><strong>' + date2 + '</strong>' + ' ' + concert2 + '</p>'
+
+            infowindow.setContent(contentString);
+            infowindow.open(denverMap, this);
+
+        });
 
 
 
         return marker;
-        });
+    });
 
     // Concerts tonight function
 
@@ -124,108 +120,125 @@ var ViewModel = function() {
 
 
 
-    function initMap() {
-        // Styles a map in night mode via Google Maps API documentation
-            
-        denverMap = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 39.7392, lng: -104.9903},
-          zoom: 13,
-          styles: [
+function initMap() {
+    // Styles a map in night mode via Google Maps API documentation
+
+    denverMap = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 39.7392, lng: -104.9903},
+        zoom: 13,
+        styles: [
             {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
             {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
             {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
             {
-              featureType: 'administrative.locality',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
+                featureType: 'administrative.locality',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#d59563'}]
             },
             {
-              featureType: 'poi',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
+                featureType: 'poi',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#d59563'}]
             },
             {
-              featureType: 'poi.park',
-              elementType: 'geometry',
-              stylers: [{color: '#263c3f'}]
+                featureType: 'poi.park',
+                elementType: 'geometry',
+                stylers: [{color: '#263c3f'}]
             },
             {
-              featureType: 'poi.park',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#6b9a76'}]
+                featureType: 'poi.park',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#6b9a76'}]
             },
             {
-              featureType: 'road',
-              elementType: 'geometry',
-              stylers: [{color: '#38414e'}]
+                featureType: 'road',
+                elementType: 'geometry',
+                stylers: [{color: '#38414e'}]
             },
             {
-              featureType: 'road',
-              elementType: 'geometry.stroke',
-              stylers: [{color: '#212a37'}]
+                featureType: 'road',
+                elementType: 'geometry.stroke',
+                stylers: [{color: '#212a37'}]
             },
             {
-              featureType: 'road',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#9ca5b3'}]
+                featureType: 'road',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#9ca5b3'}]
             },
             {
-              featureType: 'road.highway',
-              elementType: 'geometry',
-              stylers: [{color: '#746855'}]
+                featureType: 'road.highway',
+                elementType: 'geometry',
+                stylers: [{color: '#746855'}]
             },
             {
-              featureType: 'road.highway',
-              elementType: 'geometry.stroke',
-              stylers: [{color: '#1f2835'}]
+                featureType: 'road.highway',
+                elementType: 'geometry.stroke',
+                stylers: [{color: '#1f2835'}]
             },
             {
-              featureType: 'road.highway',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#f3d19c'}]
+                featureType: 'road.highway',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#f3d19c'}]
             },
             {
-              featureType: 'transit',
-              elementType: 'geometry',
-              stylers: [{color: '#2f3948'}]
+                featureType: 'transit',
+                elementType: 'geometry',
+                stylers: [{color: '#2f3948'}]
             },
             {
-              featureType: 'transit.station',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
+                featureType: 'transit.station',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#d59563'}]
             },
             {
-              featureType: 'water',
-              elementType: 'geometry',
-              stylers: [{color: '#17263c'}]
+                featureType: 'water',
+                elementType: 'geometry',
+                stylers: [{color: '#17263c'}]
             },
             {
-              featureType: 'water',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#515c6d'}]
+                featureType: 'water',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#515c6d'}]
             },
             {
-              featureType: 'water',
-              elementType: 'labels.text.stroke',
-              stylers: [{color: '#17263c'}]
+                featureType: 'water',
+                elementType: 'labels.text.stroke',
+                stylers: [{color: '#17263c'}]
             }
-          ]
-           
-          
-        });
-        
+        ]
 
 
-        
-        ko.applyBindings(new ViewModel());
-    
-     };
-
-      
-    // Uses multi select based on http://davidstutz.github.io/bootstrap-multiselect/
-    $(document).ready(function() {
-        $('#multiselect-includeSelectAllOption').multiselect({
-            enableCaseInsensitiveFiltering: true
-        });
     });
 
+
+
+
+    ko.applyBindings(new ViewModel());
+
+};
+
+
+// Uses multi select based on http://davidstutz.github.io/bootstrap-multiselect/
+$(document).ready(function() {
+    $('#multiselect-includeSelectAllOption').multiselect({
+        enableCaseInsensitiveFiltering: true
+    });
+});
+
+
+/*
+                $.getJSON("https://api.songkick.com/api/3.0/venues/" + songkick_id + "/calendar.json?apikey=a3sNs8vQ4zpgjhCU", function (data) {
+
+                    console.log("success")
+
+                    concert = data.resultsPage.results.event[0].performance[0].artist.displayName;
+                    concert1 = data.resultsPage.results.event[1].performance[0].artist.displayName;
+                    concert2 = data.resultsPage.results.event[2].performance[0].artist.displayName;
+
+                    date = data.resultsPage.results.event[0].start.date;
+                    date1 = data.resultsPage.results.event[1].start.date;
+                    date2 = data.resultsPage.results.event[2].start.date;
+
+
+                });
+*/
