131756d35a2ba9a6cf69d0a3a7cdaf05a10e13ea
Alex Hoffmann <alex@castalia.io>
2016-10-15 11:55:17 -0600

    Error handling

--- js/app.js
+++ js/app.js
@@ -10,16 +10,16 @@ var ViewModel = function() {
 
     self.venues = ko.observableArray([
 
-        {name: 'Bluebird Theater', lat: 39.7403, lng: -104.9484, id: 10459},
-        {name: 'Ogden Theater', lat: 39.7403, lng: -104.9753, id: 537},
-        {name: 'Hi Dive', lat: 39.7163, lng: -104.9879, id: 2951},
-        {name: 'Fillmore', lat: 39.7406, lng: -104.9772, id: 1009},
-        {name: 'Paramount Theatre', lat: 39.7444, lng: -104.9903, id: 1171},
-        {name: 'Larimer Lounge', lat: 39.7599, lng: -104.9838, id: 11428},
-        {name: 'Summit Music Hall', lat: 39.7533, lng: -104.9951, id: 836146},
-        {name: '3 Kings', lat: 39.7154, lng: -104.9873, id: 171428},
-        {name: 'Cervantes Ballroom', lat: 39.7545, lng: -104.9787, id: 5668},
-        {name: 'Gothic Theatre', lat: 39.6577, lng: -104.9878, id: 5229}
+        {name: "Bluebird Theater", lat: 39.7403, lng: -104.9484, id: 10459},
+        {name: "Ogden Theater", lat: 39.7403, lng: -104.9753, id: 537},
+        {name: "Hi Dive", lat: 39.7163, lng: -104.9879, id: 2951},
+        {name: "Fillmore", lat: 39.7406, lng: -104.9772, id: 1009},
+        {name: "Paramount Theatre", lat: 39.7444, lng: -104.9903, id: 1171},
+        {name: "Summit Music Hall", lat: 39.7533, lng: -104.9951, id: 836146},
+        {name: "3 Kings", lat: 39.7154, lng: -104.9873, id: 171428},
+        {name: "Cervantes Ballroom", lat: 39.7545, lng: -104.9787, id: 5668},
+        {name: "Gothic Theatre", lat: 39.6577, lng: -104.9878, id: 5229},
+        {name: "Ophelia's Electric Soapbox", lat: 39.7526, lng: -104.9920, id:2944358}
 
 
     ]);
@@ -64,6 +64,8 @@ var ViewModel = function() {
             date2 = data.resultsPage.results.event[2].start.date;
 
 
+        }).fail(function() {
+            alert("Error loading Songkick data...");
         });
 
         venue.marker = marker
